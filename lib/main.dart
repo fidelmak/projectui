d61418e2815e3da909e0a034daa95c00212ff05c
Oluborode Akintunde Paul <64453973+fidelmak@users.noreply.github.com>
2023-05-31 20:02:51 +0100

    Changes to be committed:

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,16 @@
 import 'package:flutter/material.dart';
-import 'home.dart';
+import 'package:projectui/screens/loading_screen.dart';
+// ignore: depend_on_referenced_packages
 
-void main() {
-  runApp(MyApp());
-}
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(),
+      theme: ThemeData.dark(),
+      home: LoadingScreen(),
     );
   }
 }
--- lib/model.dart
+++
@@ -1,9 +0,0 @@
-class Transaction {
-  final String name;
-
-  Transaction({required this.name});
-
-  factory Transaction.fromJson(Map<String, dynamic> json) {
-    return Transaction(name: json['name']);
-  }
-}
