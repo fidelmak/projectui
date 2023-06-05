10046b810590cc537e25f73c1f4229392e41f5e7
Oluborode Akintunde Paul <64453973+fidelmak@users.noreply.github.com>
2023-06-05 18:33:50 +0100

    Please enter the commit message for your changes. Lines starting

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:projectui/screens/loading_screen.dart';
+// ignore: depend_on_referenced_packages
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: LoadingScreen(),
+    );
+  }
+}
---
+++ lib/screens/loading_screen.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+//import 'package:geolocator/geolocator.dart';
+import '../services/location.dart';
+
+const apiKey = '21b79a9942618628f94873d4f7c15537';
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  void initState() {
+    super.initState();
+
+    getLocation();
+  }
+
+  void getLocation() async {
+    //LocationPermission permission = await Geolocator.requestPermission();
+    Location getlocate = Location();
+    await getlocate.getLocal();
+    print(getlocate.latitude);
+    print(getlocate.longitude);
+  }
+
+  void someThingLessthanTen(int n) {
+    if (n > 10) {
+      throw "error has occured";
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    String myMargin = "1";
+    double? mymarginDouble;
+
+    try {
+      mymarginDouble = double.parse(myMargin);
+    } catch (e) {
+      print(e);
+    }
+    return Scaffold(
+      body: Container(
+        margin: EdgeInsets.all(mymarginDouble ?? 30.0),
+        color: Colors.green,
+      ),
+    );
+  }
+}
